3555d7f6fc9f0072e3a308c0c549bef327f40f3f
Aldo Daniel <aldodanielsb@gmail.com>
2020-01-13 14:06:17 -0600

    rama master

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 import pjson from './../package.json'
 
-export const SERVER_URL = 'http://localhost:9000'
+export const SERVER_URL = 'http://3.18.113.46:9000'
 export const CLIENT_VERSION = pjson.version 
