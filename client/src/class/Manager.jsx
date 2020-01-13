08803293248a7007fe8bbd4ca0d06e7e483d9288
Blacked <aldodanielsb@gmail.com>
2020-01-12 18:40:31 -0600

    otro avance

--- client/src/class/Manager.jsx
+++ client/src/class/Manager.jsx
@@ -1,30 +1,98 @@
 import React, { Component } from "react";
 import './../css/Manager.css'
-import { Nav, Navbar, Form, Button } from 'react-bootstrap';
+import { Nav, Navbar, Form, Button, Modal } from 'react-bootstrap';
 import { CardServices } from './Services'
 
 class NavBar extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            ModalAdd: false,
+            ModalDel: false
+        }
+    }
+
+    handleShow = () => {
+        this.setState({ ModalAdd: true })
+    }
+
+    handleClose = () => {
+        this.setState({ ModalAdd: false })
+    }
+
+    ModalAdd(){
+        return(
+            <>  
+                <Modal show={this.state.ModalAdd} onHide={this.handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={this.handleClose}>
+                    Close
+                    </Button>
+                    <Button variant="primary" >
+                    Save Changes
+                    </Button>
+                </Modal.Footer>
+                </Modal>
+            </>
+        )
+    }
+
+    ModalRem(){
+        return(
+            <>  
+                <Modal show={this.state.ModalAdd} onHide={this.handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={this.handleClose}>
+                    Close
+                    </Button>
+                    <Button variant="primary" >
+                    Save Changes
+                    </Button>
+                </Modal.Footer>
+                </Modal>
+            </>
+        )
+    }
+
     render(){
         return(
-          <Navbar className="navbar" bg="dark" variant="dark">
-            <Navbar.Brand href="/">Service Manager</Navbar.Brand>
-              <Nav className="mr-auto">
-                <Nav.Link className="add-btn" href="/">Crear</Nav.Link>
-                <Nav.Link className="upd-btn" href="/services">Modificar</Nav.Link>
-                <Nav.Link className="rem-btn" href="/contact">Eliminar</Nav.Link>
-              </Nav>
-              <Form inline>
-              <Button variant="danger" href="/">Cerrar Sesión</Button>
-              </Form>
-        </Navbar>
+            <div>
+                <Navbar className="navbar" bg="dark" variant="dark">
+                    <Navbar.Brand href="/">Service Manager</Navbar.Brand>
+                        <Nav className="mr-auto">
+                            <Nav.Link className="add-btn" onClick={this.handleShow}>Crear</Nav.Link>
+                            <Nav.Link className="upd-btn" onClick={this.handleShow}>Modificar</Nav.Link>
+                            <Nav.Link className="rem-btn" >Eliminar</Nav.Link>
+                        </Nav>
+                    <Form inline>
+                        <Button variant="danger" href="/">Cerrar Sesión</Button>
+                    </Form>
+                </Navbar>
+                {this.ModalAdd()}
+            </div>
+            
+            
         )
     }
 }
 
+
+
 class Manager extends Component{
 
+   
+
     render(){
-        console.log(this.props)
+        document.title = 'Services Manager'
         return(
             <div>
                 <NavBar/>
